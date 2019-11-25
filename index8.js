6e20548c48fdffdecdb4cf3d60575f431608fcd2
Rafael Dos Santos <raphael_979@hotmail.com>
2019-11-25 17:17:05 -0300

    Usando o let ao invés de var no Javascript

---
+++ index8.js
@@ -0,0 +1,21 @@
+let nome='Rafael';                   //Variavel usando let.O var é mais antigo então sempre usamos o let para criar variavel
+let nome1;
+
+
+
+
+console.log('Rafa nasceu em Araçatuba');
+console.log('Em 2019 Rafa se forma em Analise de Sistemas');
+console.log('Rafa é Solteiro e não tem filhos');
+console.log('1•→_________________________________________________');
+
+console.log('Usando Variavel nome,confira abaixo:');
+console.log(nome,'nasceu em Araçatuba-SP');
+console.log('Em 2019',nome, 'se forma em Analise de Sistemas');
+console.log(nome,' é Solteiro ainda');
+console.log('2•→_________________________________________________')
+
+console.log('Posso declarar uma variavel(nome1) e não iniciar ela.Confira logo abaixo.Ela tem valor undefined');
+console.log(nome1);
+
+
